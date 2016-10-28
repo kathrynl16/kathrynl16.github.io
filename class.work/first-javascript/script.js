f917f2c8d06bc6271d59f3f86a01059209742660
kathrynl16 <kathrynlaventure@gmail.com>
2016-10-28 16:00:41 -0400

    homework doneeee

---
+++ class.work/first-javascript/script.js
@@ -0,0 +1,35 @@
+
+// document.write('Writing to the document!');
+
+// var h1 = '<h1>' + 10 * 10 + '</h1>';
+
+// var h2 = '<h2>' + 100 / 10 + '</h2>';
+
+// var h3='<h3>' + (50 - 17) + '</h3>';
+
+// var h4='<h4>' + (50 + 17) + '</h4>';
+
+// var myName='<p>Kathryn Laventure</p>';
+
+// document.write(h1);
+
+// document.write(h2);
+
+// document.write(h3);
+
+// document.write(h4);
+
+// document.write(myName)
+
+document.getElementById('text-chunk').style.color = 'red';
+
+document.querySelector('#title').style.color = 'red';
+// function hello () {
+//   var greeting = 'hello!';
+
+//   greeting = 'hello, world!';
+
+//   alert(greeting);
+// }
+
+// hello ();
