cb54dbbb78733187a8183eebb0eb70d4dd8e883d
kathrynl16 <kathrynlaventure@gmail.com>
2016-11-20 19:28:45 -0500

    send

--- final project/js/project.js
+++ final project/js/project.js
@@ -1,1 +1,73 @@
-project.js
+$(document).ready(function(){
+
+
+	// show each div when scrolls over and hides after
+	// $(window).scroll(function(){
+	// 	var scrollTop = #(window).scrollTop();
+	// 	$('.bat').css();
+		// })
+
+	// var topOfBat = $('.bat').offset().top;
+	// var topOfDog = $('.dog').offset().top;
+	// $(window).scroll(function (){
+	// 	if ($(window).scrollTop() > topOfBat && $(window).scrollTop() < topOfDog) {
+	// 		$('.bat').fadeIn("slow");
+	// 	} else {
+	// 		$('.bat').fadeOut("slow");
+	// 	}
+	// })
+	var batScrollPos = $('.bat').offset().top;
+	var dogScrollPos = $('.dog').offset().top;
+	
+	console.log(batScrollPos, dogScrollPos)
+
+
+	// $(window).scroll(function (){
+	// 	if ($(this).scrollTop() > 1200 && $(this).scrollTop() < 1900){
+	// 	    $(".bat").css('visibility', 'visible');
+	// 	    $(".dog").css('visibility', 'hidden');
+	// 	    $(".human").css('visibility', 'hidden');
+	// 	}
+ // 		else if ($(this).scrollTop() > 1900 && $(this).scrollTop() < 3000){
+	//         $(".bat").css('visibility', 'hidden');
+	//         $(".dog").css('visibility', 'visible');
+	//         $(".human").css('visibility', 'hidden');
+	// 	} 
+	// 	else {
+	// 		 $(".bat").css('visibility', 'hidden');
+	// 		 $(".dog").css('visibility', 'hidden');
+	// 		 $(".human").css('visibility', 'hidden');
+	// 	 }
+	// })
+    function fade() {
+        $('.fade').each(function() {
+            /* Check the location of each desired element */
+            var objectBottom = $(this).offset().top + $(this).outerHeight();
+            var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+            
+            /* If the object is completely visible in the window, fade it in */
+            if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+                if ($(this).css('opacity')==0) {$(this).fadeTo(500,1);}
+            } else { //object goes out of view (scrolling up)
+                if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
+            }
+        });
+    }
+    fade(); //Fade in completely visible elements during page-load
+    $(window).scroll(function() {
+    	fade();
+	}); //Fade in elements during scroll
+
+	// $('.bat').show();
+	// $('.dog').show();
+	// $('.human').show();
+	// $('.butterfly').show();
+	// $('.mantis-shrimp').show();
+
+
+
+
+
+
+
+});
