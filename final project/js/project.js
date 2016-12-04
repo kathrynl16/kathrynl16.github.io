35d9ce1f4b8999656350c5a6fb554ac38fb2fa3a
kathrynl16 <kathrynlaventure@gmail.com>
2016-12-04 15:35:24 -0500

    mobile responsive

--- final project/js/project.js
+++ final project/js/project.js
@@ -23,67 +23,23 @@ $(document).ready(function(){
 		
 		// Get window size
 	    winH = $window.height();
-		winW = $window.width();
+	    
 	    
 	    // Keep minimum height 550
 	    if(winH <= 550) {
 			winH = 550;
 		} 
-			    // Init Skrollr for 768 and up
-	    if( winW >= 768) {
-
-	        // Init Skrollr
-	        var s = skrollr.init({
-	            forceHeight: false
-	        });
-
-	        // Resize our slides
-	    $slide.height(winH);
+	    
+	    // Resize our slides
+	    $slide.height(winH*1);
 	    $slide1.height(winH*1.5)
 	    $slideTall.height(winH*2);
 	    $slideTall2.height(winH*3);
-
-	        s.refresh($('.homeSlide'));
-
-	    } else {
-
-	        // Init Skrollr
-	        var s = skrollr.init();
-	        s.destroy();
-	    }
-	
-		// Check for touch
-	   	if(Modernizr.touch) {
-
-			// Init Skrollr
-			var s = skrollr.init();
-			s.destroy();
-	   	}
-
 	    
-	    // Resize our slides
-	    // $slide.height(winH*1);
-	    // $slide1.height(winH*1.5)
-	    // $slideTall.height(winH*2);
-	    // $slideTall2.height(winH*3);
-	    
-	    // // Refresh Skrollr after resizing our sections
-	    // s.refresh($('.homeSlide'));
+	    // Refresh Skrollr after resizing our sections
+	    s.refresh($('.homeSlide'));
 	    
 	}	
-		function initAdjustWindow() {
-	    return {
-	        match : function() {
-	            adjustWindow();
-	        },
-	        unmatch : function() {
-	            adjustWindow();
-	        }
-	    };
-	}
-
-	enquire.register("screen and (min-width : 768px)", initAdjustWindow(), false)
-	        .listen(100);
 	
 	adjustWindow()
 
